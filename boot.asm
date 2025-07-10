5463c491e0e1e75e4b3432554f784ea573783eea
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-07-10 18:39:42 +0100

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,7 +1,30 @@
-; It Loops... Do I Have To Explain,
-loop:
-    jmp loop 
-
+mov ah, 0x0e ; i forgor
+mov al, 'T'
+int 0x10
+mov al, 'h'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'c'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 't'
+int 0x10
+mov al, '.'
+int 0x10
+mov al, '.'
+int 0x10
+mov al, '?'
+int 0x10
 ; + 510 Bytes
 times 510-($-$$) db 0
 ; Code For BIOS To Recognise This Operating System As Competent
