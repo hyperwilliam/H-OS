1c3bbc359c551eb8d825a1464180b2c85b67fca3
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-07-11 08:30:38 +0100

    Update boot2.asm

--- source/boot2.asm
+++ source/boot2.asm
@@ -1,7 +1,47 @@
 org 0x7E00
 bits 16
 
-; placeholder here
+ print_string:
+   lodsb        ; grab a byte from SI
+ 
+   or al, al  ; logical or AL by itself
+   jz .done   ; if the result is zero, get out
+ 
+   mov ah, 0x0E
+   int 0x10      ; otherwise, print out the character!
+ 
+   jmp print_string
+ 
+ .done:
+   ret
+ 
+ get_string:
+   xor cl, cl
+ 
+ .loop:
+   mov ah, 0
+   int 0x16   ; wait for keypress
+ 
+   cmp al, 0x08    ; backspace pressed?
+   je .backspace   ; yes, handle it
+ 
+   cmp al, 0x0D  ; enter pressed?
+   je .done      ; yes, we're done
+ 
+   cmp cl, 0x3F  ; 63 chars inputted?
+   je .loop      ; yes, only let in backspace and enter
+ 
+   mov ah, 0x0E
+   int 0x10      ; print out character
+ 
+   stosb  ; put character in buffer
+   inc cl
+   jmp .loop
+
+mov, si where
+
+where:
+db 'You Found The Secret!'
 
 ; + 2048 Bytes
 times 2048-($-$$) db 0
